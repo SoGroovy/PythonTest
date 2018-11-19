5332bc9798587aa9090ee207ef975fb18423edd7
SoGroovy <38405830+SoGroovy@users.noreply.github.com>
2018-11-19 16:30:34 -0600

    Add files via upload

--- validSentence.py
+++ validSentence.py
@@ -1,11 +1,13 @@
+#Take data and input into variable
 import re
 
-fname = 'validSentence.txt'
-fo = open(fname)
-fr = fo.read()
-# sentences = re.split(r"[A-Z]\w+[^A-Za-z0-9]*\s(\w*\s*)+(\.!?)*", fr)
-# for lines in sentences:
-#     print(lines)
-for sentences in fr:
-    for lines in re.findall('[A-Z]\w+[^A-Za-z0-9]*\s(\w*\s*)+(\.!?)*', sentences):
-        print(lines)
+fileName = 'validSentence.txt'
+fileOpen = open(fileName)
+
+#Process data and place into a different variable
+list = list()
+for lines in fileOpen:
+    list.append(re.findall('([[A-Z]\w+[^A-Za-z0-9]*\s+[\w\s]+[.!?]*)', lines))
+
+#Print
+print(list)
