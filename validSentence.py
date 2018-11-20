8c79bd4f13481a6931785b54cd7ba0eb70273da7
SoGroovy <38405830+SoGroovy@users.noreply.github.com>
2018-11-20 11:50:34 -0600

    Add files via upload

--- validSentence.py
+++ validSentence.py
@@ -3,10 +3,11 @@ import re
 
 fileName = 'validSentence.txt'
 fileOpen = open(fileName)
+fileRead = fileOpen.read()
 
 #Process data and place into a different variable
 list = list()
-for lines in fileOpen:
+for lines in fileRead:
     list.append(re.findall('([[A-Z]\w+[^A-Za-z0-9]*\s+[\w\s]+[.!?]*)', lines))
 
 #Print
